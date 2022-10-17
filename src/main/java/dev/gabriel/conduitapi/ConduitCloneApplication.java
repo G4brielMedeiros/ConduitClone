c2d094b505c5e1fca492bbe98c15f315115c6813
Gabriel Medeiros <gabriel.medeiros@liferay.com>
2022-10-17 17:31:16 -0300

    init workspace

---
+++ src/main/java/dev/gabriel/conduitapi/ConduitCloneApplication.java
@@ -0,0 +1,13 @@
+package dev.gabriel.conduitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConduitCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gabriel/conduitapi/ConduitCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabriel.conduitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduitCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
