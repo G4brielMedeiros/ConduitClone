4e2d38300fb61013bb5f5244555d7b38a6d9ddcd
Gabriel Medeiros <gabriel.medeiros@liferay.com>
2023-02-22 22:37:49 -0300

    implement email validation

--- src/main/java/dev/gabriel/conduitapi/dto/NewAccountDTO.java
+++ src/main/java/dev/gabriel/conduitapi/dto/NewAccountDTO.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import dev.gabriel.conduitapi.validation.ValidNewAccount;
 
+import javax.validation.constraints.Email;
+
 @ValidNewAccount
 @JsonTypeName("user")
 @JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
-public record NewAccountDTO(String username, String email, String password) {
+public record NewAccountDTO(String username, @Email(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$") String email, String password) {
 }
